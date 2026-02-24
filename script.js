e768766d03981dc9c4d8ed5aecb76b068a92025c
suanlee42 <leew42@stconline.edu.hk>
2026-02-24 17:30:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ buttons.forEach(button => {
         this.classList.add('selected');
     });
 });
+
+function toggleElement(){
+    const element = document.getElementById("control_panel");
+    const toggle = document.getElementById("toggle_icon");
+    if(element.style.display === "none"){
+        element.style.display = "block";
+        toggle.classList.add("rotate");
+        toggle.classList.add("toggle_active");
+    }
+    else{
+        element.style.display = "none";
+        toggle.classList.remove("rotate");
+        toggle.classList.remove("toggle_active");
+    }
+}
